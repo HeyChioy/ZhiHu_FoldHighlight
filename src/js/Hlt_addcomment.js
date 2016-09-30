45e6f09d662ecddea0c724b55cd2a6501fd15a87
Chioy <HeyChioy@foxmail.com>
2016-10-01 04:04:39 +0800

    Change 2 anonymouse function

--- src/js/Hlt_addcomment.js
+++ src/js/Hlt_addcomment.js
@@ -1,20 +1,20 @@
-console.log("Highlight Zhihu started.");
-
-var hltAddcomment = function(){
-  try {
-      if($(".focusin").length == 0){
-        $("[name='addcomment']").attr("style","");
-        $(".meta-item.toggle-comment.js-toggleCommentBox").attr("style","");
-      }else {
-        if($(".focusin a[name='addcomment']").length == 0){
-          $($(".focusin a")[1]).attr("style","color:#ffffff;border-style:solid;border-color:rgb(5, 93, 181);background-color:rgb(5, 93, 181);");
+(function(){
+  console.log("Highlight Zhihu started.");
+  var hltAddcomment = function(){
+    try {
+        if($(".focusin").length == 0){
+          $("[name='addcomment']").attr("style","");
+          $(".meta-item.toggle-comment.js-toggleCommentBox").attr("style","");
+        }else {
+          if($(".focusin a[name='addcomment']").length == 0){
+            $($(".focusin a")[1]).attr("style","color:#ffffff;border-style:solid;border-color:rgb(5, 93, 181);background-color:rgb(5, 93, 181);");
+          }
+          $(".focusin a[name='addcomment']").attr("style","color:#ffffff;border-style:solid;border-color:rgb(5, 93, 181);background-color:rgb(5, 93, 181);");
         }
-        $(".focusin a[name='addcomment']").attr("style","color:#ffffff;border-style:solid;border-color:rgb(5, 93, 181);background-color:rgb(5, 93, 181);");
-      }
-  } catch (e) {}
-}
-hltAddcomment()
-setInterval(function(){
-
+    } catch (e) {}
+  }
   hltAddcomment()
-},500)
+  setInterval(function(){
+    hltAddcomment()
+  },500)
+})();
